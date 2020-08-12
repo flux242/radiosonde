c585ccf27a062876c979f826dc809ae2936f64cd
flux242 <flux242@gmail.com>
2020-08-12 08:55:04 +0200

    ADDED: decoding with type detection

--- scripts/receivemultisonde.sh
+++ scripts/receivemultisonde.sh
@@ -105,6 +105,36 @@ decode_sonde()
     done
 }
 
+start_decoder()
+{
+  local decoder bw
+
+  case "$1" in
+    RS41) decoder="./rs41mod --ptu --ecc --crc --json -vv /dev/stdin > /dev/stderr";bw=10 ;;
+    RS92) decoder="./rs92mod -e "$EPHEM_FILE" --crc --ecc --json /dev/stdin > /dev/stderr";bw=10 ;;
+    DFM9) decoder="tee >(./dfm09mod --ptu --ecc --json /dev/stdin > /dev/stderr) >(./dfm09mod --ptu --ecc --json -i /dev/stdin > /dev/stderr)";bw=10 ;;
+     M10) decoder="./m10mod --ptu --json > /dev/stderr";bw=19.2 ;;
+       *) ;;
+  esac
+
+  ./iq_fm --lpbw $bw - 48000 32 --bo 16 |
+  sox -t raw -esigned-integer -b 16 -r 48000 - -b 8 -c 1 -t wav - highpass 10 gain +5 |
+  tee >(aplay -r 48000 -f S8 -t wav -c 1 -B 500000 &> /dev/null) |
+  $decoder
+}
+
+decode_sonde_with_type_detect()
+{
+    ./iq_client --freq $(calc_bandpass_param "$(($1-TUNER_FREQ))" "$TUNER_SAMPLE_RATE") |
+    (type=$(./dft_detect --iq - 48000 32 | awk -F':' '{print $1}'); start_decoder "$type") &>/dev/stdout |
+    while read LINE; do
+      echo "$LINE" | grep --line-buffered -E '^{' | jq --unbuffered -rcM '. + {"freq":"'"$1"'"}' | \
+      (flock 200; socat -u - UDP4-DATAGRAM:127.255.255.255:$DECODER_PORT,broadcast,reuseaddr) 200>$MUTEX_LOCK_FILE
+    done
+}
+
+
+
 decode_sonde_iqfm()
 {
   (
@@ -150,7 +180,7 @@ echo "Deactivating slot $slot with freq $freq" >> /tmp/debug.out
            # activate slot
            [ -z "${slots[$freq]}" ] && {
 echo "Activating slot $slot with freq $freq" >> /tmp/debug.out
-             decode_sonde_iqfm "$freq" &
+             decode_sonde_with_type_detect "$freq" &
              slots[$freq]=$!
            }
          fi

07ef5172655ac5353ef9cecb7ebf64052391e897
flux242 <flux242@gmail.com>
2020-08-25 10:48:30 +0200

    CHANGED: a json string is returned now

--- scripts/calcdistance.sh
+++ scripts/calcdistance.sh
@@ -2,5 +2,6 @@
 
 
 awk -i ./latlon-spherical.awk -v lat1=$1 -v lon1=$2 -v lat2=$3 -v lon2=$4 -v radius=${5:-6371000} \
-'BEGIN{print calc_distance(lat1, lon1, lat2, lon2, radius); print calc_bearing(lat1, lon1, lat2, lon2)}'
+'BEGIN{print "{\"distance\":" calc_distance(lat1, lon1, lat2, lon2, radius) ",\"bearing\":" calc_bearing(lat1, lon1, lat2, lon2) "}";
+}'
 

17fcd059f84c4eccfbde1027ca28b9880ead1aa4
flux242 <flux242@gmail.com>
2020-10-31 18:29:37 +0100

    CHANGED: timeout is set to 5 sec

--- scripts/getephemeris.sh
+++ scripts/getephemeris.sh
@@ -10,6 +10,6 @@ filename="brdc${day}0.${year}n.Z"
 outfile="${1:-$EPHEM_FILE}"
 [ "$outfile" = "-" ] && outfile='/dev/stdout'
 
-wget --timeout=3 -O/tmp/$filename "ftp://cddis.gsfc.nasa.gov/gnss/data/daily/$(date +%Y)/brdc/$filename"
+wget --timeout=5 -O/tmp/$filename "ftp://cddis.gsfc.nasa.gov/gnss/data/daily/$(date +%Y)/brdc/$filename"
 [ $? == 0 ] && compress -d -c /tmp/$filename > $outfile
   

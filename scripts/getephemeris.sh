d712827d9e0e9b183c0fda2bc7d2a394e223c386
flux242 <flux242@gmail.com>
2020-11-16 08:56:43 +0100

    CHANGED: server for ephemeris files

--- scripts/getephemeris.sh
+++ scripts/getephemeris.sh
@@ -6,10 +6,12 @@
 day=$(date +%j)
 year=$(date +%y)
 
-filename="brdc${day}0.${year}n.Z"
+#filename="brdc${day}0.${year}n.Z"
+filename="auto${day}0.${year}n"
 outfile="${1:-$EPHEM_FILE}"
 [ "$outfile" = "-" ] && outfile='/dev/stdout'
-
-wget --timeout=5 -O/tmp/$filename "ftp://cddis.gsfc.nasa.gov/gnss/data/daily/$(date +%Y)/brdc/$filename"
-[ $? == 0 ] && compress -d -c /tmp/$filename > $outfile
+wget --timeout=5 -O/tmp/$filename "ftp://lox.ucsd.edu/rinex/$(date +%Y)/$(date +%j)/$filename"
+# nasa.gov won't allow anonymous access to the RINEX broadcast ephemeris files since 31 Oct 2020
+# wget --timeout=5 -O/tmp/$filename "ftp://cddis.gsfc.nasa.gov/gnss/data/daily/$(date +%Y)/brdc/$filename"
+[ $? == 0 ] && compress -d -c /tmp/$filename > $outfile || cp /tmp/$filename $outfile
   

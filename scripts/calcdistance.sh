375a8daf16dda7e2052dfabebefa85997c74596d
flux242 <flux242@gmail.com>
2023-06-26 08:40:15 +0200

    COSMETIC: awk script path added

--- scripts/calcdistance.sh
+++ scripts/calcdistance.sh
@@ -1,7 +1,7 @@
 #!/bin//bash
 
 
-awk -i ./latlon-spherical.awk -v lat1=$1 -v lon1=$2 -v lat2=$3 -v lon2=$4 -v radius=${5:-6371000} \
+awk -i $(dirname -- "${BASH_SOURCE[0]}")/latlon-spherical.awk -v lat1=$1 -v lon1=$2 -v lat2=$3 -v lon2=$4 -v radius=${5:-6371000} \
 'BEGIN{print "{\"distance\":" calc_distance(lat1, lon1, lat2, lon2, radius) ",\"bearing\":" calc_bearing(lat1, lon1, lat2, lon2) "}";
 }'
 
--- scripts/calcintersection.sh
+++ scripts/calcintersection.sh
@@ -6,7 +6,7 @@
 #        ./calcintersection.sh 51.8853 0.2545 108.547 49.0034 2.5735 32.435 | \
 #         jq -rc 'select(.lat)|[ .lat, .lon ]|"\(.[0]);\(.[1])"' => 50.9078;4.50841
 
-awk -i ./latlon-spherical.awk -v lat1=$1 -v lon1=$2 -v brng1=$3 -v lat2=$4 -v lon2=$5 -v brng2=$6 \
+awk -i $(dirname -- "${BASH_SOURCE[0]}")/latlon-spherical.awk -v lat1=$1 -v lon1=$2 -v brng1=$3 -v lat2=$4 -v lon2=$5 -v brng2=$6 \
  'BEGIN{latlon=calc_intersection(lat1, lon1, brng1, lat2, lon2, brng2); n=split(latlon,p,";"); print "{\"lat\":"p[1]", \"lon\":"p[2]"}";
 }'
 
